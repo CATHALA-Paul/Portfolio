d68cecfb4cae545a6cf96b4e5e1038e1a8b89d49
CATHALA Paul <65656460+CATHALA-Paul@users.noreply.github.com>
2024-06-27 11:31:39 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log("salut")
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll("header nav a");
+
+    links.forEach(link => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetSection.offsetTop - document.querySelector("header").offsetHeight,
+                behavior: "smooth"
+            });
+        });
+    });
+});
